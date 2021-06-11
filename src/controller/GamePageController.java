b4456e3f46622ad067d6f3b5176695bd7f261c14
Sarnik <sarah80nik@gmail.com>
2021-06-11 14:53:57 +0430

    implement Initializable to the controller

--- src/controller/GamePageController.java
+++ src/controller/GamePageController.java
@@ -1,16 +1,26 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class GamePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GamePageController implements Initializable{
 
     @FXML
     private VBox playGround;
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
 
+
+
+    }
 
 
 
