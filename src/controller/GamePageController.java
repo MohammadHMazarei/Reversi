6208910c38ef7211c619960be8c91be1e564b0ec
MMD <mazareimohammadhosein@gmail.com>
2021-06-11 16:08:06 +0430

    Cell.java added , in GamPage.fxml line 8 prefHeigh and prefWidth changed , in GamePageController lin 26 - 36 setUpButton function created to set up buttons for table

--- src/controller/GamePageController.java
+++ src/controller/GamePageController.java
@@ -1,17 +1,37 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.Cell;
 
-public class GamePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GamePageController implements Initializable {
 
     @FXML
     private VBox playGround;
 
+    Cell[][] thisCell;
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setUpButtons();
+    }
 
 
-
-
+    //This function make 8*8 table that contain all buttons that we call each button cell
+    private void setUpButtons(){
+        for (int i = 0; i < 10; i++) {
+            HBox field = new HBox();
+            for (int j = 0; j < 10; j++) {
+                Cell cell = new Cell(i , j);
+                field.getChildren().add(cell);
+            }
+            playGround.getChildren().add(field);
+        }
+    }
 }
---
+++ src/model/Cell.java
@@ -0,0 +1,47 @@
+package model;
+
+import javafx.scene.control.Button;
+
+public class Cell extends Button {
+    //This class Extends button , this is button our material to make a 8*8 table
+    private int xPosition;
+    private int yPosition;
+
+    private boolean isSelected;
+
+
+    public Cell (int xPosition , int yPosition){
+        super(" ");
+
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+
+        setPrefHeight(100);
+        setPrefWidth(100);
+    }
+
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+}
