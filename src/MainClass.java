9ca22450602947f2bc0f50189d92e9f883a09524
Sarnik <sarah80nik@gmail.com>
2021-06-11 13:14:04 +0430

    add icon and scene to the primaryStage :)

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,42 @@
-public class MainClass {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class MainClass extends Application {
 
 
     //comment !
 
 
+    public static void main(String[] args) {
+
+      launch(args);
+    }
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/GamePage.fxml"));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            //
+        }
+        Parent root = loader.getRoot();
 
+        // Add image to icon of primaryStage Which is in the photos folder (in src)!!
+        Image icon = new Image("images/imgbin_circle-png.png");
 
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(icon);
+        primaryStage.show();
+    }
 }
--- src/controller/GamePageController.java
+++ src/controller/GamePageController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class GamePageController {
+
+    @FXML
+    private VBox playGround;
+
+
+
+
+
+
+
 }
