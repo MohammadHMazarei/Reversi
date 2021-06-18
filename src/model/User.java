98ddc7514bb2c29d061e4d4e2b7b987a1fadb772
Sarnik <sarah80nik@gmail.com>
2021-06-18 22:38:03 +0430

    add class user for log in

--- src/model/User.java
+++ src/model/User.java
@@ -1,2 +1,46 @@
-package model;public class User {
+package model;
+
+public class User {
+
+
+    private String userName;
+    private String password;
+    private int point ;
+
+
+    public User(String userName , String password , int point){
+
+        this.password = password;
+        this.userName = userName;
+        this.point = point;
+
+    }
+
+    public User(){
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
